a036f8668ffd17aa740d1fb37b4841611061f898
sophiecogswell24@hotmail.com Cogswell <sophiecogswell24@hotmail.com>
2018-07-30 18:44:46 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -8,24 +8,23 @@ function init() {
 
   body.addEventListener("keydown",function(e) {
     alert("I was keydowned!");
+  })
 
-  
-})
 
 let index = 0;
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
  
   if (key === alphabet[index]) {
-    index++;
- 
+      index++;
+  }
     if (index === alphabet.length) {
       alert("Hurray!");
       index = 0;
   }
   
   console.log(body);
-}
+ }
 }
 
 init()
