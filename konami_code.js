e48cc16c54b9f70fa6b8436504adee57a76db26e
sophiecogswell24@hotmail.com Cogswell <sophiecogswell24@hotmail.com>
2018-07-30 18:47:41 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,29 +3,31 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 console.log(code)
 
 function init() {
+ // Write your JavaScript code inside the init() function
 
-  const body = document.querySelector("body");
+ const body = document.querySelector(“body”)
 
-  body.addEventListener("keydown",onKeyDownHandler(this))
+ let index = 0;
 
+ body.addEventListener(“keydown”,
+   function(e) {
+     const key = parseInt(e.detail || e.which);
+   
+    console.log(code)
+   
+     if (key === code[index]) {
+       index++;
+     }
+     
+     if (index === code.length) {
+         alert(“Hurray!“);
+         index = 0;
+     }
+   }
+ )
 
-let index = 0;
-function onKeyDownHandler(e) {
-  const key = parseInt(e.detail || e.which);
- 
-  if (key === alphabet[index]) {
-      index++;
-  }
-    if (index === alphabet.length) {
-      alert("Hurray!");
-      index = 0;
-  }
-  
-  console.log(body);
- }
+ console.log(body);
 }
 
 init()
- 
-
 
