f8e0205d42bb6ba608e5a471f86ca56fe7434648
sophiecogswell24@hotmail.com Cogswell <sophiecogswell24@hotmail.com>
2018-07-30 18:29:49 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,11 +2,12 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 console.log(code)
 
-function init() {
+function init(e) {
   
 }
 
 let index = 0;
+
 function init() {
   
 const body = document.querySelector("body");
