ad583205f9c3211e23086555e8104fe99a5591aa
sophiecogswell24@hotmail.com Cogswell <sophiecogswell24@hotmail.com>
2018-07-30 18:31:00 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -19,6 +19,23 @@ console.log(event);
   
 })
 
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+ 
+  if (key === alphabet[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
+
+
  
 
 
